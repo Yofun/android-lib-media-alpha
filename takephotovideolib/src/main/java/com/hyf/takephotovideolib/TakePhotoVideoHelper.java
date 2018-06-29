abd5afd4a0062b6cf39d812a80a70bccfddcfb05
丨Rainbow丨 <775183940@qq.com>
2018-06-29 10:31:44 +0800

    初始化上传

---
+++ takephotovideolib/src/main/java/com/hyf/takephotovideolib/TakePhotoVideoHelper.java
@@ -0,0 +1,15 @@
+package com.hyf.takephotovideolib;
+
+/**
+ * Created by  HYF on 2018/6/29.
+ * Email：775183940@qq.com
+ */
+
+public class TakePhotoVideoHelper {
+    public interface Mode {
+        // 三种模式
+        int RECORD_MODE_PHOTO = 0;
+        int RECORD_MODE_VIDEO = 1;
+        int RECORD_MODE_ALL = 2;
+    }
+}
